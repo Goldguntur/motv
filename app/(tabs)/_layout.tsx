6b491382abec777a350e340217c5eb121c4771a3
gunturPutra <goldguntur1623@gmail.com>
2024-07-19 00:56:53 +0700

    menghapus component bawaan dan mengubah tabs screen dengan home, search, favorite start dan setting serta menambahkan file nya, mengubah font nebjadi montserrat

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,115 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Image, Text, View } from "react-native";
+import icons from "../../constants/icons";
 
+interface TabIconProps {
+  icon: any;
+  color: string;
+  name: string;
+  focused: boolean;
+}
+
+const TabIcon = ({ icon, color, name, focused }: TabIconProps) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-semibold" : "font-regular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#f01e2c",
+          tabBarInactiveTintColor: "#fff",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#121214",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: "Search",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.search}
+                color={color}
+                name="Search"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="favorite"
+          options={{
+            title: "Favorite",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.favorite}
+                color={color}
+                name="Favorite"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="setting"
+          options={{
+            title: "Setting",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.setting}
+                color={color}
+                name="Setting"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   );
 }
---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import { ImageRequireSource } from "react-native";
+
+// constants\icons.ts
+const eye: ImageRequireSource = require("../assets/images/eye.png");
+const eyeHide: ImageRequireSource = require("../assets/images/eye-hide.png");
+const search: ImageRequireSource = require("../assets/images/search.png");
+const setting: ImageRequireSource = require("../assets/images/setting-icon.png");
+const favorite: ImageRequireSource = require("../assets/images/favorite-star.png");
+const home: ImageRequireSource = require("../assets/images/home.png");
+
+export default { eye, eyeHide, search, setting, favorite, home };
